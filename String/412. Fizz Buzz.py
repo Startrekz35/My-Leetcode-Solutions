64fa4a61655980240eaecbc7b29e8218b6304246
Startrekz35 <burkz3905@outlook.com>
2024-10-27 20:23:47 -0500

    Commenting on my code

--- String/412. Fizz Buzz.py
+++ String/412. Fizz Buzz.py
@@ -1,7 +1,9 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        ans = []
+        ans = [] #array to store
+        #N + 1 to have the last number inclusive
         for i in range(1, n + 1):
+            #takes care of divisors
             if i % 3 == 0 and i % 5 == 0:
                 ans.append("FizzBuzz")
             elif i % 3 == 0:
@@ -9,5 +11,6 @@ class Solution:
             elif i % 5 == 0:
                 ans.append("Buzz")
             else:
+                #Includes quotes while maintaining the variable
                 ans.append(f"{i}")
         return ans
